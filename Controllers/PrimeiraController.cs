0b6ff78ba298098e2c1a574dfa2ab1295907f9a0
FIC <FIC@lab_08_ws25.info.local>
2018-01-16 21:11:11 -0200

    Primeiro commit

---
+++ Controllers/PrimeiraController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebServiceCidades.Controllers
+{
+    public class PrimeiraController:Controller
+    {
+        [HttpGet]
+        public void Primeira()
+        {
+            
+        }
+    }
+}
